cb823d3447b597bbf08c6c6b91dd05a31d7cda03
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-11-18 18:51:26 +0000

    last fun die and 3 core

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!-- @author Stephen O'Connor -->
+<html>
+   <head>
+      <meta charset="UTF-8">
+      <title>My Website</title>
+   </head>
+   <body>
+      <h1>My Website</h1>   
+      <?php $name = "Stephen O'Connor"; 
+      echo "<p>Hello $name. Is your name $name?</p>";?> 
+   </body>
+</html>
