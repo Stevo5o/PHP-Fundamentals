1688809f481df1924e1f4329357b6a7e11f804a2
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-11-18 20:01:21 +0000

    Arrays

--- index.php
+++ index.php
@@ -1,13 +1,23 @@
+<?php
+$fav_sites = [
+    'stephen o\'Connor' => 'http://stephenoconnor.azurewebsites.net',
+    'imdb' => 'http://imdb.com',
+    'metacritic' => 'http://metacritic.com'
+];
+?>
 <!DOCTYPE html>
 <!-- @author Stephen O'Connor -->
 <html>
    <head>
       <meta charset="UTF-8">
-      <title>My Website</title>
+      <title>Arrays</title>
    </head>
    <body>
-      <h1>My Website</h1>   
-      <?php $name = "Stephen O'Connor"; 
-      echo "<p>Hello $name. Is your name $name?</p>";?> 
+      <h1>My Sites</h1>   
+      <?php
+      foreach ($fav_sites as $site => $url) {
+         echo "<li><a href='$url' target=_blank>" . ucwords($site) . "</a></li>";
+      }
+      ?> 
    </body>
 </html>
