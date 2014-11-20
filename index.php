0baa19ae65674baa1b1d6264b0e74d1d907049b4
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-11-20 12:31:07 +0000

    Arrays print_r  <pre> & specific element

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ $fav_sites = [
     'imdb' => 'http://imdb.com',
     'metacritic' => 'http://metacritic.com'
 ];
+
+$months = ['jan', 'feb', 'mar', 'april', 'may'];
+
+//$month[0] = 'jan';
 ?>
 <!DOCTYPE html>
 <!-- @author Stephen O'Connor -->
@@ -14,11 +18,29 @@ $fav_sites = [
    </head>
    <body>
       <h1>My Sites</h1>   
-      <?php
-      foreach ($fav_sites as $site => $url) : ?>
-         <li>
-            <a href="<?= $url; ?>"target=_blank><?= ucwords($site); ?></a>
-                </li>
-                <?php endforeach ?>
-   </body>
+      <?php foreach ($fav_sites as $site => $url) : ?>
+      <li>
+         <a href="<?= $url; ?>"target=_blank><?= ucwords($site); ?></a> // new echo assoc array
+      </li>
+   <?php
+   endforeach;
+   echo "<br />";
+   
+   // echo first second and last months
+   echo reset($months);
+   echo "<br />";
+   echo next($months);
+   echo "<br />";
+   echo end($months);
+
+   // print specific month and array of months
+   echo '<pre>';
+   echo "This is the 3rd month: ";
+   print_r(ucwords($months[2]));
+   echo "</pre><pre>";
+   print_r($months);
+   echo '</pre>';
+   ?>
+
+</body>
 </html>
