228ab769198d6711a96dc43772c4d208bb27173b
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-11-20 14:00:44 +0000

    Arrays push, pop, shift, unshift, slice, filter -lambda

--- index.php
+++ index.php
@@ -7,7 +7,20 @@ $fav_sites = [
 
 $months = ['jan', 'feb', 'mar', 'april', 'may'];
 
-//$month[0] = 'jan';
+$months[] = 'june'; // array_push
+$months[] = 'july'; // array_push
+
+$month1 = array_shift($months); // remove first 
+array_unshift($months, 'jan'); // unremove first
+
+$month = array_pop($months); // remove last item from array - store pop in month
+
+$monthsSlice = array_slice($months, 2); // slice out after 2
+
+$monthsSliceFebMar = array_slice($months, 1, 2); // slice out after 1 and take 2
+
+$month3Char = array_filter($months, function($item) { return strlen($item) == 3; }); // filter with lamda function
+
 ?>
 <!DOCTYPE html>
 <!-- @author Stephen O'Connor -->
@@ -18,29 +31,44 @@ $months = ['jan', 'feb', 'mar', 'april', 'may'];
    </head>
    <body>
       <h1>My Sites</h1>   
-      <?php foreach ($fav_sites as $site => $url) : ?>
-      <li>
-         <a href="<?= $url; ?>"target=_blank><?= ucwords($site); ?></a> // new echo assoc array
-      </li>
-   <?php
-   endforeach;
-   echo "<br />";
-   
-   // echo first second and last months
-   echo reset($months);
-   echo "<br />";
-   echo next($months);
-   echo "<br />";
-   echo end($months);
+         <?php foreach ($fav_sites as $site => $url) : ?>
+         <li>
+            <a href="<?= $url; ?>"target=_blank><?= ucwords($site); ?></a> <!-- new echo assoc --> 
+         </li>
+         <?php
+      		endforeach;
+      echo "<br />";
 
-   // print specific month and array of months
-   echo '<pre>';
-   echo "This is the 3rd month: ";
-   print_r(ucwords($months[2]));
-   echo "</pre><pre>";
-   print_r($months);
-   echo '</pre>';
-   ?>
+      // echo first second and last months
+      echo "Months in months array <br /> First: " . reset($months);
+      echo "<br />";
+      echo "Second: " . next($months);
+      echo "<br />";
+      echo "Pushed & Last: " . end($months);
 
-</body>
+      // print specific month and array of months
+      echo '<pre>';
+      echo "This is the 3rd month: ";
+      print_r(ucwords($months[2]));
+      echo "</pre><pre>";
+      echo "Months ";      
+      print_r($months);       
+      echo '</pre>';
+      echo "This last month: <b>" . ucwords($month) . "</b> has been removed <br />";
+      echo "This first month: <b>" . ucwords($month1) . "</b> has been removed <br />";
+      echo "This first month: <b>" .  ucwords($months[0]) . "</b> has been unremoved <br />";
+      echo "<pre>"; 
+      echo "Slice after 2, Months ";     
+      print_r($monthsSlice);    
+      echo '</pre>';
+      echo "<pre>"; 
+      echo "Slice out months <b>Feb & Mar</b> ";     
+      print_r($monthsSliceFebMar);    
+      echo '</pre>'; 
+      echo "<pre>"; 
+      echo "Filter months with only <b>3 chars </b>";     
+      print_r($month3Char);    
+      echo '</pre>';           
+      ?>
+   </body>
 </html>
