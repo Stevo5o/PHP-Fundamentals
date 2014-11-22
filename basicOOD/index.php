e6179ea22e1b64f251e82c8dbdea66f8712c5841
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-11-22 15:30:11 +0000

    PHP write / test my code online

---
+++ basicOOD/index.php
@@ -0,0 +1,57 @@
+<?php
+
+class Person {
+
+   public $name;
+   public $job;
+
+   public function __construct($name, $job) {
+      $this->name = $name;
+      $this->job = $job;
+   }
+
+   public function communicate($style = 'voice') {
+      return 'communicating ' . $style;
+   }
+
+}
+
+$p = new Person('John', 'Teacher');
+var_dump($p);
+echo $p->communicate('telepathy');
+
+// generic object - empty class
+$person1 = new stdClass;
+$person1->first = "John";
+$person1->last = "Doe";
+$person1->job = "Teacher";
+
+echo "<br />" . $person1->first . ' ' . $person1->last;
+
+$person2 = [
+    'first' => "Stephen",
+    'last' => 'O\'Connor'
+];
+
+var_dump($person2);
+
+echo '<pre>';
+print_r($person2);
+echo '</pre>';
+
+// cast array to an object
+var_dump((object) $person2);
+
+echo '<pre>';
+print_r((object) $person2);
+echo '</pre>';
+
+echo $person2['first'] . "\n" . $person2['last'];
+echo "<br />";
+
+$o = (object) $person2;
+echo $o->first . "\n" . $o->last;
+echo "<br />";
+
+// cast string to interger
+echo gettype((int) '5');
---
+++ functions/index.php
@@ -0,0 +1,51 @@
+<?php
+
+function say_hello($name = 'buddy') {
+	return "Hi, there $name.";
+}
+
+function pp($value) {
+echo '<pre>';
+print_r($value);
+echo '</pre>';
+}
+
+function array_pluck($toPluck, $arr) {
+	$ret = [];
+
+	foreach ($arr as $item) {
+		$ret[] = $item[$toPluck];
+	}
+
+	return($ret);
+}
+
+function array_pluck2($toPluck, $arr) {
+	return array_map(function($item) use($toPluck){
+		return $item[$toPluck];
+	}, $arr);
+}
+
+$greeting = say_hello('Ste');
+
+echo $greeting;
+
+$arr = ['name' => 'Ste', 'age' => '38', 'occupation' => 'Web Developer'];
+
+pp($arr);
+
+$people = [
+    ['name' => 'Ste', 'age' => '38', 'occupation' => 'Web Developer'],
+    ['name' => 'John', 'age' => '40', 'occupation' => 'Teacher'],
+    ['name' => 'Jane', 'age' => '30', 'occupation' => 'Marketing']    
+    ];
+
+$plucked = array_pluck('name', $people); // array = ['Ste', 'John', 'Jane'];
+
+print_r($plucked);
+echo "<br />";
+
+$names = array_pluck2('name', $people);
+	foreach($names as $name) {
+		echo "$name\n";
+	}
