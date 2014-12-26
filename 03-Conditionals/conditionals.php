2ee6699f7cba7d46454605c4daea91d3d98ceb5c
Stephen J. O'Connor <stevo.joc@gmail.com>
2014-12-26 12:28:53 +0000

    conditionals with Regex

--- 03-Conditionals/conditionals.php
+++ 03-Conditionals/conditionals.php
@@ -1,5 +1,5 @@
 <?php
-$month = 'January';
+$month = 'December';
 ?>
 <!DOCTYPE html>
 <!-- @author Stephen O'Connor -->
@@ -11,50 +11,56 @@ $month = 'January';
    </head>
    <body>
       <h2>Conditionals</h2>
-         <?php         
-         switch ($month) {
-            case 'January':
-               # code...
-               echo "Switch: It is Jan! <br />";
-               break;
+      <?php
+      switch ($month) {
+         case 'January':
+            echo "Switch: It is Jan! <br />";
+            break;
 
-            case 'Febuary':
-               echo "It is Feb!";
-               break;
+         case 'Febuary':
+            echo "It is Feb!";
+            break;
 
-            case 'March':
-               echo "It is Mar!";
+         case 'March':
+            echo "It is Mar!<br>";
+            break;
 
-            default:
-               # code...
-               echo "Not the right month";
-               break;
-         }
-         
-         // look-up
-         $months = [
-         'January' 	=> 'This is January',
-         'Feburary' => 'This is Febuary',
-         'March' 	=> 'This is March'
-         ];
+         default:
+            echo "Not the right month<br>";
+            break;
+      }
 
-         echo "Look-up: " . $months[$month] . "<br />";
-         echo "Ternary: ";
-         echo isset($months[$month]) ? $months[$month] : 'Not the right month!';
-         echo "<br />";
-         // two AND conditionals
-         if ($month !== 'January' && $month !== 'June') {
-         	echo "It's not Jan or June";
-         } else {
-         	echo "It is one of those";
-         }
-         echo "<br />";
-          // two OR conditionals
-         if ($month === 'May' || $month === 'June') {
-         	echo "It is one of those";
-         } else {
-         	echo "It is not May or June";
-         }
-         ?>  
+      // look-up
+      $months = [
+          'January' => 'This is January',
+          'December' => 'This is December',
+          'Feburary' => 'This is Febuary',
+          'March' => 'This is March'
+      ];
+
+      echo "Look-up: " . $months[$month] . "<br />";
+      echo "Ternary: ";
+      echo isset($months[$month]) ? $months[$month] : 'Not the right month!';
+      echo "<br />";
+      // two AND conditionals
+      if ($month !== 'January' && $month !== 'June') {
+         echo "It's not Jan or June";
+      } else {
+         echo "It is one of those";
+      }
+      echo "<br />";
+      // two OR conditionals
+      if ($month === 'May' || $month === 'June') {
+         echo "It is one of those";
+      } else {
+         echo "It is not May or June<br>";
+      }
+
+      // regular expressions http://webcheatsheet.com/php/regular_expressions.php
+      if (preg_match("/ell/", "Hello World!", $matches)) {
+         echo "Match was found <br />";
+         echo $matches[0];
+      }
+      ?>
    </body>
 </html>
